7a069f7db398887af371b6b7e7165d42af19272c
mangoshi <mangoshimonks@gmail.com>
2021-12-02 13:33:38 +0000

    Added work from class last week

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 Vue.use(BootstrapVue)
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
--- src/pages/projects/AllProjects.vue
+++ src/pages/projects/AllProjects.vue
@@ -1,18 +1,51 @@
 <template>
-	<div>
-		This is the AllProjects component.
-		<hr>
-		<input type="text" v-model="searchTerm">
-		<div
-		v-for="project in filteredProjects"
-		:key="project.id"
-		>
+	<div class="columns">
+		<div class="column">
+			<b-field label="Search Projects">
+				<b-input v-model="searchTerm"/>
+			</b-field>
+
+			<div class="card" v-for="project in filteredProjects" :key="project.id">
+				<div class="card-image">
+					<figure class="image is-4by3">
+						<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+					</figure>
+				</div>
+				<div class="card-content">
+					<div class="media">
+						<!-- <div class="media-left">
+							<figure class="image is-48x48">
+								<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+							</figure>
+						</div> -->
+						<div class="media-content">
+							<p class="title is-4">{{project.title}}</p>
+							<b-taglist>
+								<b-tag v-for="tag in project.tags" :key="tag" type="is-info">{{ tag }}</b-tag>
+							</b-taglist>
+							<p class="subtitle is-6">{{project.description}}</p>
+							Date: {{ project.date }} <br>
+							<p v-if="project.demo"> Demo: <router-link :to="{name: project.demo}">Demo</router-link></p>
+						</div>
+					</div>
+				<div class="content">
+					<!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+					<a href="#">#css</a> <a href="#">#responsive</a>
+					<br>
+					<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time> -->
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!-- 
 		Title: {{ project.title }} <br>
 		Description: {{ project.description }} <br>
 		Date: {{ project.date }} <br>
-		Demo: <router-link :to="{name: project.demo}">Demo</router-link>
+		<p v-if="project.demo"> Demo: <router-link :to="{name: project.demo}">Demo</router-link></p>
 		<hr>
-		</div>
+		</div> -->
 	</div>
 </template>
 
@@ -33,7 +66,7 @@ export default {
 	computed:{
 		filteredProjects(){
 			return this.projects.filter(project  => {
-				return project.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+				return project.description.toLowerCase().includes(this.searchTerm.toLowerCase())
 			})
 		}
 	},
